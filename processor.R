09582aa0865f969e4cd3c07956cf7f6d7323df31
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2017-07-16 15:15:42 +0100

    Rename vars, update plan, add query code
    Rename vars to a more sensible name
    Update explanation for how to work through the data
    Add query to grab a subset of the data

--- processor.R
+++ processor.R
@@ -1,19 +1,19 @@
 # Program to process and analyse financial data for herding
 
 ## Read in CSV Data
-iss.csv <- read.csv("InstitutionSecuritiesDataset.csv", header = TRUE)
+iss.data <- read.csv("InstitutionSecuritiesDataset.csv", header = TRUE) # This is a data frame
 str(iss.csv) # Show structure (str)
 
 # Consolidate institutions into unique list - useful for looping through
-institutions <- factor(iss.csv$InstitutionID)
+institutions <- factor(iss.data$InstitutionID)
 institutionLevels <- levels(institutions)
 
 # Consolidate security tickers into unique list
-securities <- factor(iss.csv$SecurityTicker)
+securities <- factor(iss.data$SecurityTicker)
 securityTickerLevels <- levels(securities)
 
 # Get dates and convert into date format - not necessary but may be useful
-dates <- factor(iss.csv$ReportDate)
+dates <- factor(iss.data$ReportDate)
 dateLevels <- levels(dates)
 quarterDates <- as.Date(dateLevels, "%Y%m%d") # Convert into date objects
 
@@ -21,7 +21,7 @@ dateFormat <- format(quarterDates, "%Y%b%d") # Not used, just an example of form
 
 # Create a template data.frame to hold B or S for each security per institution - then add together after
 # Currently need a data.frame for B and another for S...could consolidate into a single data.frame?
-securityTemplateBS <- data.frame(B = rep(0, times = length(dateTest)), S = rep(0, times = length(dateTest)), quarter = dateTest)
+securityTemplateBS <- data.frame(B = rep(0, times = length(quarterDates)), S = rep(0, times = length(quarterDates)), quarter = quarterDates)
 # securityChangeHold[[4,1]] # Test to access data in 4th row of first column
 # securityChangeHold[[4,1]] <- 1 # Test to alter field in 4th row of first column
 securityTemplateBS
@@ -29,19 +29,30 @@ securityTemplateBS
 # Quick test to see if new object is created or reference (new object! so altering it doesn't alter the original)
 testSecTemplate <- securityTemplateBS
 # testSecTemplate[5,1] <- 5 # Change row 5 column 1 field value to 5
+testSecTemplate[5,] # Return row 5, all 3 columns - don't need to set to return the row
 testSecTemplate
 
-for(i in institutionLevels) {
-  print(i)
-}
-
+# This will form the main loop which will work through each security and delegate specific parts to other functions
 for(i in securityTickerLevels) {
   print(i)
 }
 
-# Okay lets do a test run with UNP and then loop the rest of them
-# Loop through Institutions, grab from the iss.csv or table or whatever the rows
-# that equal the current institution and security, then create a vector/matrix and populate with B and S values
-# Store it? hm.. but then do the same again for each institution and add together,
-# maybe add at the end of each loop?
+# Okay lets do a test run with UNP and then integrate the rest of them using above loop
+# Grab all rows for the current security and put in a data frame subset
+# Loop through Institutions, grab subset the current institution and work out B & S for each year & store in vector
+# add to prior vector using R's cool vector math
+# After B & S are gathered for the security can then feed into the AF formula
+# After the AF formula is done can feed into herding formula
+
+# Grab all rows from the data frame where security ticker equals UNP - similar to an SQL query
+indices <- which(iss.data$SecurityTicker == "UNP")
+dataSubset <- iss.data[indices, ]
+dataSubset
+
+for(i in institutionLevels) {
+  print(i)
+  # which(iss.data$InstitutionID == i & iss.data$SecurityTicker == "UNP") # might not work well
+}
+
+
 
